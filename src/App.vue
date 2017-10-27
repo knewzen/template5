3b0e9ddec0c96b3684da1b72b2303b0debfa0118
Mateusz Różalski <olivetum@gmail.com>
2017-10-27 08:51:36 +0200

    Add main msg line && change fWork linear gradient

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,39 @@
 <template>
   <div id="app">
-      <div class="page-head-bg">
 
-      </div>
+      <div class="page-head-bg"></div>
+
       <div class="page-head flex-center">
           <div class="banner container">
           <div class="logo" >
             <img class="logo-img" src="./assets/img/logo.svg">
             <div style="width: 100%" class="wordmark"></div>
           </div>
+        </div>
 
 
 
         </div>
 
-
-
-          <div style="position: absolute; top: 220px;">
-            <div class="menu">
-              <a v-for="item in menu">{{item.name}}</a>
-            </div>
-            <div class="works">
-              <FeaturedWork/>
-            </div>
+        <div class="works">
+          <div class="menu-container">
+            <div class="message"><p>HI</p></div>
+            <div class="menu"><a v-for="item in menu">{{item.name}}</a></div>
           </div>
 
+          <FeaturedWork/>
         </div>
-
-
-
   </div>
 </template>
 
 <script>
 import Navigation from './assets/components/Navigation.vue';
 import FeaturedWork from './assets/components/FeaturedWork.vue'
+import Portfolio from './assets/components/Portfolio.vue'
 
 export default {
   name: 'app',
-  components: {Navigation, FeaturedWork},
+  components: {Navigation, FeaturedWork, Portfolio},
   data () {
     return {
       msg: 'Lorem',
