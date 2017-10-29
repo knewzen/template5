ab4cf3936a9baf3f0dd7a8c7a9f3bcea0376d224
Mateusz Różalski <olivetum@gmail.com>
2017-10-29 19:06:30 +0100

    Add modal component && modalShow event

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -8,25 +8,39 @@
 
     <div  v-for="item in works"
           class="work-box box shadow flex-center FeaturedWorkBackground"
-          :style="{ background: item.background}">
-          <div class="work-description">
-
-          </div>
+          :style="{ background: item.background}"
+          @click="item.modalShow = !item.modalShow">
+          <div class="work-description"></div>
+          <ShowcaseModal v-if="item.modalShow" />
       </div>
     </div>
+
   </div>
 </template>
 
 <script>
+  import ShowcaseModal from './ShowcaseModal.vue'
   export default {
+    components: {ShowcaseModal},
     data () {
       return  {
+        show: false,
         works: [
-          {background: "url('./src/assets/img/tk.jpg')"},
-          {background: "url('./src/assets/img/tk.jpg')"},
-          {background: "url('./src/assets/img/tk.jpg')"}
+          {background: "url('./src/assets/img/tk.jpg')",
+          modalShow: false},
+          {background: "url('./src/assets/img/tk.jpg')",
+          modalShow: false},
+          {background: "url('./src/assets/img/tk.jpg')",
+          modalShow: false},
+          {background: "url('./src/assets/img/tk.jpg')",
+          modalShow: false},
         ]
       }
+    },
+    methods: {
+      toggleModal: function () {
+          this.show = !this.show
+        }
     }
   }
 </script>
