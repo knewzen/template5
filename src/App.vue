53344f66df6b6c84dd43e6b556eafc2503b6fa1e
Mateusz Różalski <olivetum@gmail.com>
2017-10-29 10:12:11 +0100

    Add work atrubiute

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
 
       <div class="page-head-bg"></div>
+      <div class="page-head flex-center">
 
       <!-- LOGO -->
-      <div class="page-head flex-center">
           <div class="banner container">
           <div class="logo" >
             <img class="logo-img" src="./assets/img/logo_w.svg">
@@ -13,18 +13,17 @@
         </div>
       </div> <!-- LOGO END -->
 
-        <div class="works">
+        <div class="portfolio">
           <!-- Menu -->
           <div>
             <p class="message">Orci varius natoque penatibus <br>et magnis dis parturient montes, <br>nascetur ridiculus mus.</p>
           </div>
           <div class="menu-container">
-
-
             <div class="menu"><a v-for="item in menu">{{item.name}}</a></div>
           </div> <!-- Menu End -->
 
           <FeaturedWork/>
+          <Works/>
         </div>
   </div>
 </template>
@@ -32,11 +31,12 @@
 <script>
 import Navigation from './assets/components/Navigation.vue';
 import FeaturedWork from './assets/components/FeaturedWork.vue'
-import Portfolio from './assets/components/Portfolio.vue'
+import Works from './assets/components/Works.vue'
+
 
 export default {
   name: 'app',
-  components: {Navigation, FeaturedWork, Portfolio},
+  components: {Navigation, FeaturedWork, Works, Works},
   data () {
     return {
       msg: 'Lorem',
---
+++ src/assets/components/Works.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="works">
+    <div v-for="item in works" class="work-box box shadow flex-center FeaturedWorkBackground">{{msg}}</div>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return  {
+        msg: 'Hello World',
+        works: [
+          {},
+          {},
+          {},
+          {},
+          {},
+
+        ]
+      }
+    }
+  }
+</script>
