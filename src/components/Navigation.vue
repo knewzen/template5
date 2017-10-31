45921d9a2def8c33b91a82fe3dcc8fb8d145db28
Mateusz Różalski <olivetum@gmail.com>
2017-10-31 11:30:46 +0100

    Add toggle menu js function

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -6,7 +6,7 @@
       <p class="message">Orci varius natoque penatibus <br>et magnis dis parturient montes, <br>nascetur ridiculus mus.</p>
     </div>
     <div class="menu-container">
-      <div class="menu"><a v-for="item in menu">{{item.name}}</a></div>
+      <div class="menu"><a id="menuItem" v-for="item in menu">{{item.name}}</a></div>
     </div> <!-- Menu End -->
   </div>
 </template>
