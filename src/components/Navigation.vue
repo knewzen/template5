7a81951cfb2d8585a8eaf7c0fd49ebb77b28cb82
Mateusz Różalski <olivetum@gmail.com>
2017-10-31 12:56:57 +0100

    Add animation to navbar

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-
-    <button id="menuButton">menu</button>
     <div>
       <p class="message">Orci varius natoque penatibus <br>et magnis dis parturient montes, <br>nascetur ridiculus mus.</p>
     </div>
+
+    <div class="navbar">
+      <button id="menuButton"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></button>
+    </div>
+
+
+
     <div class="menu-container">
       <div class="menu"><a class="effect-shine" v-for="item in menu">{{item.name}}</a></div>
     </div> <!-- Menu End -->
